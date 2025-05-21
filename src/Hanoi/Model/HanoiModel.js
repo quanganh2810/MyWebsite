ff6a90e36590ad6f97944c0d333874c5c53c6e81
QuangAnh <anhquang1028@gmail.com>
2025-05-21 13:47:00 +0700

    Render version

--- src/Hanoi/Model/HanoiModel.js
+++ src/Hanoi/Model/HanoiModel.js
@@ -1,12 +1,13 @@
-// models/destinationModel.js
+const BASE_URL = 'https://mywebsitebackend-cpsk.onrender.com';
+
 export async function fetchItemsByPlace(place) {
-  const res = await fetch(`http://localhost:5059/api/destination/items-by-place/${place}`);
+  const res = await fetch(`${BASE_URL}/api/destination/items-by-place/${place}`);
   if (!res.ok) throw new Error('Failed to fetch items');
   return res.json();
 }
 
 export async function fetchAddressByItem(item) {
-  const res = await fetch(`http://localhost:5059/api/destination/address-by-item/${encodeURIComponent(item)}`);
+  const res = await fetch(`${BASE_URL}/api/destination/address-by-item/${encodeURIComponent(item)}`);
   if (!res.ok) throw new Error('Failed to fetch address');
   return res.json();
 }
